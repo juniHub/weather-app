257b8fcbcace66f93ce106e7423899318261420d
Thuy Tien Nguyen <35412770+juniHub@users.noreply.github.com>
2020-10-05 10:50:53 -0700

    some change

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+
+const api = {
+  key: process.env.REACT_APP_WEATHER_KEY,
+  base: 'https://api.openweathermap.org/data/2.5/',
+};
+
+function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = (evt) => {
+    if (evt.key === 'Enter') {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  };
+
+  return (
+    <div
+      className={
+        typeof weather.main != 'undefined'
+          ? weather.main.temp > 16
+            ? 'app warm'
+            : 'app cold'
+          : 'app'
+      }
+    >
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Enter the city"
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {typeof weather.main != 'undefined' ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{new Date().toDateString()}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°C</div>
+              <div className="weather">{weather.weather[0].description}</div>
+              <div className="icon">
+                <img
+                  src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                />
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="error">
+            Enter your city name to know the current weather!
+          </div>
+        )}
+      </main>
+    </div>
+  );
+}
+
+export default App;
