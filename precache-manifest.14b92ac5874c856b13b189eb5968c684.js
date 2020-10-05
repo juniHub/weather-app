91ffa22bf14fe98328393afa95ca71cca18f3949
Thuy Tien Nguyen <35412770+juniHub@users.noreply.github.com>
2020-10-05 11:07:19 -0700

    Updates

---
+++ precache-manifest.14b92ac5874c856b13b189eb5968c684.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03c3ad6bc7e0494a3e527d854243a852",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "4474c3ec05f88412c223",
+    "url": "/weather-app/static/css/main.a644de2d.chunk.css"
+  },
+  {
+    "revision": "4a79708fe2bb8d98f2f2",
+    "url": "/weather-app/static/js/2.07144284.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.07144284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4474c3ec05f88412c223",
+    "url": "/weather-app/static/js/main.1bc42add.chunk.js"
+  },
+  {
+    "revision": "933af7f4ac4be227ef6e",
+    "url": "/weather-app/static/js/runtime-main.88d99ec0.js"
+  },
+  {
+    "revision": "9e9d4d89c0a63d231bb52d690fe7838d",
+    "url": "/weather-app/static/media/cold.9e9d4d89.svg"
+  },
+  {
+    "revision": "d343b35760feeb8bb1243413fc7e9b7b",
+    "url": "/weather-app/static/media/warm.d343b357.svg"
+  },
+  {
+    "revision": "7abe21a8cbf10d290e39e80ce6314d3b",
+    "url": "/weather-app/static/media/weather-cover.7abe21a8.svg"
+  }
+]);
