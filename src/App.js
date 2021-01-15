310f84bd9b1ad62e209e88e65895867b9960d51d
tcaovo <40118714+tcaovo@users.noreply.github.com>
2021-01-15 04:34:53 -0800

    some improvement

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Advise from './Advise';
 
-
 const api = {
   key: process.env.REACT_APP_WEATHER_KEY,
   base: 'https://api.openweathermap.org/data/2.5/',
@@ -10,8 +9,15 @@ const api = {
 function App() {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
+  const [value,setValue] = useState('Celsius');
 
+  const handleChange = (event) => {
+
+    setValue(event.target.value);
+    setWeather({});
   
+  }
+
 
   const searchC = (evt) => {
     if (evt.key === 'Enter') {
@@ -27,7 +33,7 @@ function App() {
 
   const searchF = (evt) => {
     if (evt.key === 'Enter') {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
         .then((res) => res.json())
         .then((result) => {
           setWeather(result);
@@ -41,32 +47,40 @@ function App() {
     <div
       className={
         typeof weather.main != 'undefined'
-          ? weather.main.temp > 16
+          ? (weather.main.temp > 16 && value==="Celsius") ||  (weather.main.temp > 60 && value==="Fahrenheit")
             ? 'app warm'
             : 'app cold'
           : 'app'
       }
     >
       <main>
-        <div className="search-box">
+
+  <div className="select-box">
+   <label className="prompt">
+    Select Celsius or Fahrenheit:
+    <select className="select" value={value} onChange={handleChange}>
+      <option value="Celsius">Celsius</option>
+      <option value="Fahrenheit">Fahrenheit</option>
+    </select>
+    </label>
+    </div>
+ 
+      <div className="search-box">
+        
           <input
             type="text"
             className="search-bar"
             placeholder="Enter the city name"
             onChange={(e) => setQuery(e.target.value)}
             value={query}
-            onKeyPress={searchC}
+            onKeyPress={value==="Celsius"?searchC:searchF}
           />
-          <div>Or Click here to show C or F degree</div>
-           <button>
-             Search Celsius
-           </button>
-           <button>
-             Search Fahrenheit
-           </button> 
 
-        </div>
-        {typeof weather.main != 'undefined' ? (
+          </div>
+
+      
+
+        {typeof weather.main !== 'undefined' ? (
           <div>
             <div className="location-box">
               <div className="location">
@@ -76,7 +90,7 @@ function App() {
              
             </div>
             <div className="weather-box">
-              <div className="temp">{Math.round(weather.main.temp)}°C</div>
+              <div className="temp">{Math.round(weather.main.temp)} {value==="Celsius"? '°C':'℉'}</div>
               <div className="weather">{weather.weather[0].description}</div>
               <div className="icon">
                 <img alt="weather-icon"
@@ -92,8 +106,8 @@ function App() {
 
           </div>
         ) : (
-          <div className="error">
-            Enter the city name to know the current weather!
+          <div className="prompt">
+            Enter the city name to know the current weather and get tip of the day!
           </div>
         )}
 
